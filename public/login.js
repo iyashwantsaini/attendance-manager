3a35b342470a3f9dafa651970824cf1ece937f17
Glitch (puzzling-potassium) <none>
2019-08-31 16:16:22 +0000

    🔋🎨 Checkpoint ./public/login.js:6055465/2244:6056050/372

--- public/login.js
+++ public/login.js
@@ -19,37 +19,67 @@ studloginsub.addEventListener('click', (e) => {
   
   auth.signInWithEmailAndPassword(email,pass).then(function(){
   console.log("login_success!!");
-    
-    var user = firebase.auth().currentUser;
-
+  
+  var user = firebase.auth().currentUser;
   if (user != null) {
     user.providerData.forEach(function (profile) {
-      console.log("Sign-in provider: " + profile.providerId);
-      console.log("  Provider-specific UID: " + profile.uid);
-      console.log("  Name: " + profile.displayName);
       console.log("  Email: " + profile.email);
-      console.log("  Photo URL: " + profile.photoURL);
     });
-  }
-    
+  }    
   }).catch(function(error) {
-  auth.createUserWithEmailAndPassword(email, pass).then(function (){
-  var database = firebase.database();
-  var name="yash";
-  var email="yash@yash.com";
-  firebase.database().ref('users').push({
-    username: name,
-    email: email });
-  }).catch(function(error) {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log("error_!!");
-  });
   var errorCode = error.code;
   var errorMessage = error.message;
   console.log(errorMessage);
   });
+
 });
+
+teachloginsub.addEventListener('click', (e) => {
+  e.preventDefault();
+  var email=studmail.value;
+  var pass=studpass.value;
+  var auth=firebase.auth();
+  
+  auth.signInWithEmailAndPassword(email,pass).then(function(){
+  console.log("login_success!!");
+  
+  var user = firebase.auth().currentUser;
+  if (user != null) {
+    user.providerData.forEach(function (profile) {
+      console.log("  Email: " + profile.email);
+    });
+  }    
+  }).catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(errorMessage);
+  });
+
+});
+
+adminloginsub.addEventListener('click', (e) => {
+  e.preventDefault();
+  var email=studmail.value;
+  var pass=studpass.value;
+  var auth=firebase.auth();
+  
+  auth.signInWithEmailAndPassword(email,pass).then(function(){
+  console.log("login_success!!");
+  
+  var user = firebase.auth().currentUser;
+  if (user != null) {
+    user.providerData.forEach(function (profile) {
+      console.log("  Email: " + profile.email);
+    });
+  }    
+  }).catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(errorMessage);
+  });
+
+});
+
 //   auth.onAuthStateChanged(function(user) {
 //   if (user) {
 //     console.log("user logged in!");
@@ -78,3 +108,35 @@ studloginsub.addEventListener('click', (e) => {
 //   });
 
 // });
+
+
+
+  
+  // auth.createUserWithEmailAnd
+
+
+
+  
+  auth.createUserWithEmailAndPassword(email, pass).then(function (){
+  var database = firebase.database();
+  var name="yash";
+  var email="yash@yash.com";
+  firebase.database().ref('users').push({
+    username: name,
+    email: email });
+  }).catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log("error_!!");
+  });Password(email, pass).then(function (){
+  // var database = firebase.database();
+  // var name="yash";
+  // var email="yash@yash.com";
+  // firebase.database().ref('users').push({
+  //   username: name,
+  //   email: email });
+  // }).catch(function(error) {
+  // var errorCode = error.code;
+  // var errorMessage = error.message;
+  // console.log("error_!!");
+  // });
