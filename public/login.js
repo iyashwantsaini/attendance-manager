66b0a0e291e928b2a11c26fc72168260ae206da4
Glitch (puzzling-potassium) <none>
2019-08-31 15:41:21 +0000

    🌠🖐 Checkpoint ./public/login.js:6055465/1290

--- public/login.js
+++ public/login.js
@@ -16,48 +16,44 @@ studloginsub.addEventListener('click', (e) => {
   var email=studmail.value;
   var pass=studpass.value;
   var auth=firebase.auth();
-  
   auth.signInWithEmailAndPassword(email,pass).then(function(){
+  console.log("login_success!!")
   user = firebase.auth().currentUser;
-  if(user!=null){
-    console.log("logged_in_success");
-  }else{
-  auth.createUserWithEmailAndPassword(email, pass).catch(function(error) {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  console.log("error_!!");
-  }).then(function (){
+  }).catch(function(error) {
+  auth.createUserWithEmailAndPassword(email, pass).then(function (){
   var database = firebase.database();
   var name="yash";
   var email="yash@yash.com";
   firebase.database().ref('users').push({
     username: name,
     email: email });
-  });
-  }
   }).catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log("error_!!");
+  });
   var errorCode = error.code;
   var errorMessage = error.message;
   console.log(errorMessage);
   });
   
-  
-  auth.onAuthStateChanged(function(user) {
-  if (user) {
-    console.log("user logged in!");
-     var database = firebase.database();
-  var name="yash";
-  var email="yash@yash.com";
-  firebase.database().ref('users').push({
-    username: name,
-    email: email });
-  } else {
-    console.log("not found eror!");
-  });
-    // User is signed out.
-  }
-  });
 });
+//   auth.onAuthStateChanged(function(user) {
+//   if (user) {
+//     console.log("user logged in!");
+//      var database = firebase.database();
+//   var name="yash";
+//   var email="yash@yash.com";
+//   firebase.database().ref('users').push({
+//     username: name,
+//     email: email });
+//   } else {
+//     console.log("not found eror!");
+//   });
+//     // User is signed out.
+//   }
+//   });
+// });
 
 // studlogoutsub.addEventListener('click', (e) => {
 //   e.preventDefault();
