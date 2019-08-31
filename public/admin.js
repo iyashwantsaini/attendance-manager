8577cfc559756dbd7b6e06e32b22f971b6585651
Glitch (puzzling-potassium) <none>
2019-08-31 17:38:20 +0000

    🍬🐺 Checkpoint ./public/admin.js:6055465/770 ./views/adminPortal.html:6055465/61 ./public/timer.js:6056050/400:6266541/27

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,14 @@
-var teachadd=document.getElementById("teach");
-var adminmail=document.getElementById("adminmail");
+var teachadd=document.getElementById("teachadd");
+var studadd=document.getElementById("studadd");
+
+var teachfirst=document.getElementById("teacher_first_name");
+var teachlast=document.getElementById("teacher_last_name");
+var teachmail=document.getElementById("teacher_code");
+var teachcode=document.getElementById("teacher_email");
+
+var teachfirst=document.getElementById("teacher_first_name");
+var teachlast=document.getElementById("teacher_last_name");
+var teachmail=document.getElementById("teacher_code");
+var teachcode=document.getElementById("teacher_email");
+
+var studentyear=document.getElementById("year").value;
