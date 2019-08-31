646116191851405cc8bcbf0b4ddfdf62c5610b8e
Glitch (puzzling-potassium) <none>
2019-08-31 23:24:13 +0000

    🍙🕴 Checkpoint ./views/matrix1.html:6056050/483 ./views/teacherPortal.html:6056050/160 ./public/matrix1.js:6056050/456 ./public/matrix2.js:6056050/494

---
+++ public/matrix2.js
@@ -0,0 +1,26 @@
+var n=1;
+			function timer(){
+			var date=new Date();
+			console.log(date);
+
+			var seconds=date.getSeconds();
+				console.log(seconds);
+	 		var t=setInterval(function(){
+			var newDate=new Date().getSeconds();
+			if(newDate-seconds>5){
+			clearInterval(t);
+			document.getElementById("demo").innerHTML="";
+			
+				clearInterval(t);
+
+		}
+		console.log(newDate);
+		n++;
+
+	}
+	,1000);
+
+	};
+	timer();
+	export default arr1;
+	
