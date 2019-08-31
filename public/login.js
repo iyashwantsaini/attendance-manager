6f96f0342bfb4d90d0268f9435c75217d163518a
Glitch (puzzling-potassium) <none>
2019-08-31 16:04:24 +0000

    💕🍈 Checkpoint ./public/landing.css:6055465/28:6056050/255 ./views/admin.html:6055465/1 ./public/login.js:6055465/548

--- public/login.js
+++ public/login.js
@@ -16,9 +16,22 @@ studloginsub.addEventListener('click', (e) => {
   var email=studmail.value;
   var pass=studpass.value;
   var auth=firebase.auth();
+  
   auth.signInWithEmailAndPassword(email,pass).then(function(){
-  console.log("login_success!!")
-  user = firebase.auth().currentUser;
+  console.log("login_success!!");
+    
+    var user = firebase.auth().currentUser;
+
+  if (user != null) {
+    user.providerData.forEach(function (profile) {
+      console.log("Sign-in provider: " + profile.providerId);
+      console.log("  Provider-specific UID: " + profile.uid);
+      console.log("  Name: " + profile.displayName);
+      console.log("  Email: " + profile.email);
+      console.log("  Photo URL: " + profile.photoURL);
+    });
+  }
+    
   }).catch(function(error) {
   auth.createUserWithEmailAndPassword(email, pass).then(function (){
   var database = firebase.database();
