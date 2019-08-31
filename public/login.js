b9d5386ec2d479a1658f4410cc09db2830f4a31e
Glitch (puzzling-potassium) <none>
2019-08-31 09:57:12 +0000

    🐇🍬 Checkpoint ./views/admin.html:6056050/905:6439967/778 ./public/firebase.js:6055465/624 ./views/landing.html:6055465/2027 ./public/login.js:6055465/820

---
+++ public/login.js
@@ -0,0 +1,12 @@
+var studmail=document.getElementById("#studmail");
+var teachmail=document.getElementById("#teachmail");
+var adminmail=document.getElementById("#adminmail");
+var studpass=document.getElementById("#studpass");
+var teachpass=document.getElementById("#teachpass");
+var adminpass=document.getElementById("#adminpass");
+
+var studloginsub=document.getElementById("studsubmit");
+var teachloginsub=document.getElementById("teachsubmit");
+var adminloginsub=document.getElementById("adminsubmit");
+
+studloginsub.addEventListener('click',)
