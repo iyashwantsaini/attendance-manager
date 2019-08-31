8921cc9795d09c8373afb444d008a06d4809a842
Glitch (puzzling-potassium) <none>
2019-08-31 23:36:12 +0000

    🌎🚖 Checkpoint ./views/matrix1.html:6056050/2702 ./public/matrix1.js:6056050/689

--- public/matrix1.js
+++ public/matrix1.js
@@ -1,3 +1,29 @@
+
+		var arr1 = new Array(10);
+		for (var i = 0; i < arr1.length; i++) {
+			arr1[i] = new Array(10);
+		}
+
+
+
+
+		var t = function () {
+			var table = document.getElementById("demo");
+			for (var i = 0; i < 10; i++) {
+				var row = document.createElement("tr");
+				for (var j = 0; j < 10; j++) {
+
+					var col = document.createElement("td");
+					arr1[i][j] = (i * 10) + j + 1;
+					col.innerHTML = (i * 10) + j + 1; // assigning value to the cell
+					col.style.fontSize = "50px";
+					row.appendChild(col);
+				}
+				table.appendChild(row);
+			}
+		};
+		window.onload = t;
+
 var n=1;
 			function timer(){
 			var date=new Date();
